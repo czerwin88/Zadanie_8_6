2319e01992e775b2227d52a3594cad7d848f1c7f
Czerwin <r.j.czerwinski@gmail.om>
2017-06-02 14:29:17 +0200

    Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 var a = 4;
-  b = 10;
-  value = (a * a) + (2 * a * b) - (b * b);
+var b = 10;
+var value = (a * a) + (2 * a * b) - (b * b);
 
-  if (value > 0) {
-    console.log('Wynik jest dodatni')
-    alert('Wynik jest dodatni');
+  /*if (value > 0) {
+    console.log('Wynik jest dodatni');
   }  else if (value < 0) {
-     console.log('Wynik jest ujemny')
-     alert('Wynik jest ujemny');
+     console.log('Wynik jest ujemny');
   }  else if (value == 0) {
-      console.log('Wynik to 0')
-     alert('Wynik to 0');
-  }
+      console.log('Wynik to 0');
+  }*/
 
-  console.log(value)
+console.log(value > 0 ? 'Wynik jest dodatni'  : 'Wynik jest ujemny' );
+
+console.log(value);
