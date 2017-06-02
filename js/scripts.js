95c8eb0c67828275b655ed2be8f2025c1033bd4c
Czerwin <r.j.czerwinski@gmail.om>
2017-06-02 22:59:03 +0200

    Fixed again

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,5 @@ var a = 4;
 var b = 10;
 var value = (a * a) + (2 * a * b) - (b * b);
 
-  /*if (value > 0) {
-    console.log('Wynik jest dodatni');
-  }  else if (value < 0) {
-     console.log('Wynik jest ujemny');
-  }  else if (value == 0) {
-      console.log('Wynik to 0');
-  }*/
-
-console.log(value > 0 ? 'Wynik jest dodatni'  : 'Wynik jest ujemny' );
-
+console.log(value === 0 ? 'Zero' : (value > 0 ? 'Wynik jest dodatni' : 'Wynik jest ujemny'));
 console.log(value);
